4fd00aa5fc385043605f494f729ab761545e371e
Josinosle <>
2024-07-30 22:50:28 +0100

    init

---
+++ auto_screen_brightness.py
@@ -0,0 +1,85 @@
+import os
+import time
+import configparser
+
+def backlight_target(AmbientBrightness, CurrentBacklight, config):
+
+    CurrentBacklight = int(CurrentBacklight) #Convert backlight string to an int to work with
+
+    Target = int(float(AmbientBrightness)*float(config[0])) #Backlight target aims for configurable proportion of ambient brightness
+    
+    print(AmbientBrightness)
+
+    if Target > CurrentBacklight:
+        Increment = 1 
+    elif Target < CurrentBacklight:
+        Increment = -1
+    else:
+        Increment = 0
+    
+    NewBrightness = CurrentBacklight + Increment
+
+    if NewBrightness < int(config[1]):
+        return config[1]
+    else:
+        return str(NewBrightness)
+
+def read_ambient_light():
+    with open("/sys/bus/iio/devices/iio:device0/in_illuminance_raw", "r") as AmbientBrightnessFile:
+        AmbientBrightness = AmbientBrightnessFile.read()
+        AmbientBrightnessFile.close()
+    
+    return AmbientBrightness
+
+def read_backlight_brightness():
+    global DisplayDriver
+    with open("/sys/class/backlight/" + DisplayDriver + "/actual_brightness", "r") as BacklightLevelFile:
+        BacklightBrightness = BacklightLevelFile.read()
+        BacklightLevelFile.close()
+    return BacklightBrightness
+
+def set_backlight_brightness(NewBrightness):
+    global DisplayDriver
+    os.system("echo " + str(NewBrightness) + " | sudo tee /sys/class/backlight/"+DisplayDriver+"/brightness > /dev/null")
+
+def change_backlight(config):
+    AmbientBrightness = read_ambient_light()
+    BacklightBrightness = read_backlight_brightness()
+    BacklightTarget = backlight_target(AmbientBrightness,BacklightBrightness,config)
+    set_backlight_brightness(BacklightTarget)
+
+def user_configs():
+    try:
+        config = configparser.ConfigParser()
+
+        config.read("auto_brightness.conf")
+        BrightnessScaling = config["DEFAULT"]["brightness_scaling"] 
+        MinimumBrightness = config["DEFAULT"]["minimum_brightness"]
+
+        RuntimeConfigs = [BrightnessScaling,MinimumBrightness]
+        return RuntimeConfigs
+    except:
+        print("Config file unavailable: creating new")
+        
+        config = configparser.ConfigParser()
+        config['DEFAULT'] = {"brightness_scaling": "1",
+                             "minimum_brightness": "100"}
+        with open("auto_brightness.conf", "w") as configfile:
+            config.write(configfile)
+
+"""
+Init
+"""
+DisplayDriver = os.popen("ls /sys/class/backlight").read().strip("\n")
+config = user_configs()
+print("Display Driver:",DisplayDriver,"\nUser Configs:\nScreen brightness factor:",config[0])
+
+
+"""
+Main runtime loop
+"""
+print("Script Running")
+while True:
+    change_backlight(config)
+    time.sleep(0.1)
+quit
